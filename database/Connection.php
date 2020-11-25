d131db71c221432ddcfcf9a640fc38fc87a2060f
christianotieno <oduorchristian@gmail.com>
2020-11-26 01:08:26 +0300

    add config tricks

--- database/Connection.php
+++ database/Connection.php
@@ -1,11 +1,18 @@
 <?php
 
+
 class Connection {
   
-  public static function make() {
+  public static function make($config) {
     try {
-  
-      return new PDO('mysql:host=localhost;dbname=mytodo', 'christianotieno', 'Foothill431@'); 
+
+      return new PDO(
+        $config['connection'].';dbname='.$config['name'],
+        $config['username'],
+        $config['password'],
+        $config['options']
+
+      );
     
     } catch (PDOExeption $e) {
     
